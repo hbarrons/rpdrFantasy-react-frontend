8e8b6a93bf85131cc78e92028a0d8780c76ad6ee
hbarrons <hbarrons@gmail.com>
2022-04-20 10:45:56 -0400

    add content to create league form and add form data to state

--- src/pages/CreateLeague/CreateLeague.jsx
+++ src/pages/CreateLeague/CreateLeague.jsx
@@ -1,9 +1,34 @@
-
+import { useState } from 'react'
 
 const CreateLeague = (props) => {
+  const [formData, setFormData] = useState({
+    leagueName: '',
+  })
+
+  const handleChange = evt => {
+    setFormData({
+      ...formData,
+      [evt.target.name]: evt.target.value,
+    })
+  }
+
+  const handleSubmit = async evt => {
+    console.log(evt)
+  }
+
   return ( 
     <>
-      <h1>Create League Here</h1>
+      <h1>Create League Here*</h1>
+      <p>*You will be the admin of the new league</p>
+      <form
+      autoComplete="off"
+      onSubmit={handleSubmit}
+      className="create-league-form"
+      >
+        <label htmlFor="leagueName">League Name:</label>
+        <input type="text" autoComplete="off" id="legueName" name="leagueName"
+        onChange={handleChange}/>
+      </form>
     </>
    );
 }
