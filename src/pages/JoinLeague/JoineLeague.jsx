7b2c5b60c472983b516c08736c3c7c4c4884d499
hbarrons <hbarrons@gmail.com>
2022-04-20 10:51:42 -0400

    complete join league page and add form data to state

--- src/pages/JoinLeague/JoineLeague.jsx
+++ src/pages/JoinLeague/JoineLeague.jsx
@@ -1,9 +1,38 @@
-
+import { useState } from "react"
 
 const JoinLeague = (props) => {
+  const [formData, setFormData] = useState({
+    leagueName: '',
+    leagueNo: ''
+  })
+
+  const handleChange = evt => {
+    setFormData({
+      ...formData,
+      [evt.target.name]: evt.target.value,
+    })
+  }
+
+  const handleSubmit = async evt => {
+    console.log(evt)
+  }
+
   return ( 
     <>
-      <h1>Join a League</h1>
+      <h1>Join League Here*</h1>
+      <p>*You will need the League Number from the admin in order to join an existing league</p>
+      <form
+      autoComplete="off"
+      onSubmit={handleSubmit}
+      className="create-league-form"
+      >
+        <label htmlFor="leagueName">League Name:</label>
+        <input type="text" autoComplete="off" id="legueName" name="leagueName"
+        onChange={handleChange}/>
+        <label htmlFor="leagueNo">League Number:</label>
+        <input type="text" autoComplete="off" id="legueNo" name="leagueNo"
+        onChange={handleChange}/>
+      </form>
     </>
    );
 }
