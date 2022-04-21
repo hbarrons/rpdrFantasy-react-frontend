135bbcfa9344cc2d675349f83d89abd094d95eab
hbarrons <hbarrons@gmail.com>
2022-04-20 23:21:57 -0400

    add my league page and add information to page

--- src/components/LeagueCard/LeagueCard.jsx
+++ src/components/LeagueCard/LeagueCard.jsx
@@ -1,9 +1,11 @@
 
 
-const LeagueCard = (props) => {
+const LeagueCard = ({ profile }) => {
   return ( 
     <>
       <h1>League Info Here</h1>
+      {console.log(profile.league)}
+      <h1>{profile.league[0]?.name}</h1>
     </>
    );
 }
--- src/pages/MyLeagues/MyLeagues.jsx
+++ src/pages/MyLeagues/MyLeagues.jsx
@@ -1,9 +1,16 @@
+import LeagueCard from "../../components/LeagueCard/LeagueCard"
 
 
-const MyLeague = (props) => {
+const MyLeague = ({ profiles, user }) => {
   return ( 
     <>
       <h1>My League Info</h1>
+      {console.log(profiles)}
+      {console.log(user.profile)}
+      {profiles.map(profile => {
+        return <LeagueCard profile={profile}/>
+      })}
+
     </>
    );
 }
