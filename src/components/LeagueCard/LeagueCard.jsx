b06c5b2a58a71253d2ee1e764093288982c9f1cf
hbarrons <hbarrons@gmail.com>
2022-04-21 10:03:29 -0400

    add conditional rendering for league info based on admin rights

--- src/components/LeagueCard/LeagueCard.jsx
+++ src/components/LeagueCard/LeagueCard.jsx
@@ -4,7 +4,18 @@ const LeagueCard = ({ profile, user }) => {
   return ( 
     <>
       {profile._id === user.profile ? 
-        <h1>{profile.league[0]?.name}</h1>
+        <>
+          <h3>League Name: {profile.league[0]?.name}</h3>
+          <h3>League Number: {profile.league[0]?.leagueNo}</h3>
+          {profile.league[0]?.isAdmin ? 
+            <>
+              <h3>League Password: {user.profile}</h3>
+            </>
+          :
+            <></>
+          }
+          
+        </>
         :
         <></>
       }
