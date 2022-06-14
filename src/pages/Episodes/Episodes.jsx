9537bb8168a95434f73ce5bf43ea70cb6f8d7d84
hbarrons <hbarrons@gmail.com>
2022-06-14 16:15:21 -0400

    successfully delete score from databased when respective episode is deleted

--- src/pages/Episodes/Episodes.jsx
+++ src/pages/Episodes/Episodes.jsx
@@ -149,7 +149,7 @@ const Episodes = ({ profiles, user }) => {
     console.log("episodeCount: ", episodeCount)
     if (episodeCount === episodeNum + 1) {
       try {
-        const data = await profileService.deleteScores(episodeNum, leagueNumber)
+        const data = await profileService.deleteScores(episodeCount, leagueNumber)
         console.log("delete score response: ", data)
       } catch (err) {
         console.log(err)
