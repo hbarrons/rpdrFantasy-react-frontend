11b7e3020eb6e8b07f889e03fa02314e7924eb50
hbarrons <hbarrons@gmail.com>
2022-06-24 10:26:18 -0400

    add link to player profile from myleague view

--- src/components/AdminPlayerCard/AdminplayerCard.jsx
+++ src/components/AdminPlayerCard/AdminplayerCard.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Link } from "react-router-dom";
 
 
 const AdminPlayerCard = ({profile, user, makeAdmin, removeAdmin }) => {
@@ -8,6 +9,7 @@ const AdminPlayerCard = ({profile, user, makeAdmin, removeAdmin }) => {
 
   return ( 
     <>
+    <Link to={`/profile/${profile._id}`} state={profile}>
       <li>{profile.name}
       {profile.league[0].isAdmin === false ?
       <><button className="btn" onClick={()=>makeAdmin(profile._id)}>Make Admin</button></>
@@ -15,6 +17,7 @@ const AdminPlayerCard = ({profile, user, makeAdmin, removeAdmin }) => {
       <><button className="btn btn-danger" onClick={()=>removeAdmin(profile._id)}>Remove Admin</button></>
       }
       </li>
+      </Link>
     </>
   );
 }
--- src/components/PlayerCard/PlayerCard.jsx
+++ src/components/PlayerCard/PlayerCard.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Link } from "react-router-dom";
 
 
 const PlayerCard = ({profile, user, makeAdmin}) => {
@@ -6,7 +7,9 @@ const PlayerCard = ({profile, user, makeAdmin}) => {
 
   return ( 
     <>
-      <li>{profile.name}</li>
+      <Link to={`/profile/${profile._id}`} state={profile}>
+        <li>{profile.name}</li>
+      </Link >
     </>
   );
 }
