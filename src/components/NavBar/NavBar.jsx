a1a020acd3fe44752b2327720975456e4b4cba01
hbarrons <hbarrons@gmail.com>
2022-07-08 12:25:18 -0400

    update collapsible navbar issue

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -30,16 +30,15 @@ const NavBar = ({ user, handleLogout }) => {
                   <button
                     className="navbar-toggler"
                     type="button"
-                    data-toggle="collapse"
                     data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
                     aria-expanded="false"
                     aria-label="Toggle navigation"
                   >
                     <span className="navbar-toggler-icon"></span>
                   </button>
-                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                  <div className="collapse navbar-collapse" id="navbarNav">
                     <div className='container-fluid'>
                       <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
                         <li nav-item="true" className='nav-item'><Link to="" onClick={handleLogout} className='nav-link active'>LOG OUT</Link></li>
@@ -52,35 +51,34 @@ const NavBar = ({ user, handleLogout }) => {
               </>
             } else {
              return <>
-                <nav className="navbar navbar-expand-lg navbar-light ">
-                <div className='container-fluid'>
-                  <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                  >
-                    <span className="navbar-toggler-icon"></span>
-                  </button>
-                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <div className='container-fluid'>
-                      <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-                        <li nav-item="true" className='nav-item'><Link to="/myleague" className='nav-link active'>My League</Link></li>
-                        <li nav-item="true" className='nav-item'><Link to="/queens" className='nav-link active'>Queens</Link></li>
-                        <li nav-item="true" className='nav-item'><Link to="/episodes" className='nav-link active'>Episodes</Link></li>
-                        <li nav-item="true" className='nav-item'><Link to="/mypicks" className='nav-link active'>Make Picks</Link></li>
-                        <li nav-item="true" className='nav-item'><Link to="/profiles" className='nav-link active'>Scoreboard</Link></li>
-                        <li nav-item="true" className='nav-item'><Link to="" onClick={handleLogout} className='nav-link active'>LOG OUT</Link></li>
-                        {/* <li nav-item="true"><Link to="/changePassword" className='nav-link active'>Change Password</Link></li> */}
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </nav>
+  <nav className="navbar navbar-expand-lg navbar-light ">                
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      // data-bs-toggle="collapse"
+      // data-bs-target="#navbarNav"
+      aria-controls="#navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <div className='container-fluid' id="navbarNav">
+        <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+          <li nav-item="true" className='nav-item'><Link to="/myleague" className='nav-link active'>My League</Link></li>
+          <li nav-item="true" className='nav-item'><Link to="/queens" className='nav-link active'>Queens</Link></li>
+          <li nav-item="true" className='nav-item'><Link to="/episodes" className='nav-link active'>Episodes</Link></li>
+          <li nav-item="true" className='nav-item'><Link to="/mypicks" className='nav-link active'>Make Picks</Link></li>
+          <li nav-item="true" className='nav-item'><Link to="/profiles" className='nav-link active'>Scoreboard</Link></li>
+          <li nav-item="true" className='nav-item'><Link to="" onClick={handleLogout} className='nav-link active'>LOG OUT</Link></li>
+          {/* <li nav-item="true"><Link to="/changePassword" className='nav-link active'>Change Password</Link></li> */}
+        </ul>
+      </div>
+    </div>
+</nav>
               </>
             }
           }
